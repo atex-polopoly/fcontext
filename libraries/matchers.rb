60f275ee3dd34727cc27f25c662ba5c75edc8a30
Hector Wallen <hwallen@atex.com>
2018-01-22 15:28:13 +0100

    cleaned some style, added relabel to public api, and removed some unused matchers

--- libraries/matchers.rb
+++ libraries/matchers.rb
@@ -7,13 +7,4 @@ if defined?(ChefSpec)
   def delete_selinux_policy_fcontext(resource_name)
     ChefSpec::Matchers::ResourceMatcher.new(:selinux_policy_fcontext, :delete, resource_name)
   end
-
-  def modify_selinux_policy_fcontext(resource_name)
-    ChefSpec::Matchers::ResourceMatcher.new(:selinux_policy_fcontext, :modify, resource_name)
-  end
-
-  def addormodify_selinux_policy_fcontext(resource_name)
-    ChefSpec::Matchers::ResourceMatcher.new(:selinux_policy_fcontext, :addormodify, resource_name)
-  end
-
 end
--- providers/default.rb
+++ providers/default.rb
@@ -5,6 +5,7 @@ def whyrun_supported?
   true
 end
 
+# Produces a string containing a Shell command pipeline that tells you if a given fcontext is defined
 def fcontext_defined(file_spec, file_type, label = nil)
   file_hash = {
     'a' => 'all files',
@@ -46,35 +47,60 @@ end
 def set_fcontext (not_if, only_if, commands, secontext, file_spec_suffix )
   execute "selinux-fcontext-#{new_resource.secontext}-set" do
     command "/usr/sbin/semanage fcontext #{commands} #{secontext} '#{new_resource.file_spec}#{file_spec_suffix}'"
-    if not_if != nil
-      not_if not_if
-    end
-    if only_if != nil
-      only_if only_if.to_s
-    end
+    not_if not_if unless not_if.nil?
+    only_if only_if.to_s unless only_if.nil?
     only_if { use_selinux }
     notifies :relabel, new_resource, :immediate
   end
 end
 
 action :set do
-    add_not_if = fcontext_defined(new_resource.file_spec, new_resource.file_type, new_resource.secontext)
-    set_fcontext(add_not_if, true, "-a #{semanage_options(new_resource.file_type)} -t", new_resource.secontext, '')
+    not_if = fcontext_defined(new_resource.file_spec,
+                              new_resource.file_type,
+                              new_resource.secontext)
+    set_fcontext(not_if,
+                 true,
+                 "-a #{semanage_options(new_resource.file_type)} -t",
+                 new_resource.secontext,
+                 '')
 
     if new_resource.recursive
-      add_recursive_not_if = fcontext_defined("#{new_resource.file_spec}(/.*)?", new_resource.file_type, new_resource.secontext)
-      set_fcontext(add_recursive_not_if, new_resource.recursive, "-a #{semanage_options(new_resource.file_type)} -t", new_resource.secontext, '(/.*)?')
+      recursive_not_if = fcontext_defined("#{new_resource.file_spec}(/.*)?",
+                                          new_resource.file_type,
+                                          new_resource.secontext)
+      set_fcontext(recursive_not_if,
+                   true,
+                   "-a #{semanage_options(new_resource.file_type)} -t",
+                   new_resource.secontext,
+                   '(/.*)?')
     else
-      delete_recursive_only_if = fcontext_defined("#{new_resource.file_spec}(/.*)?", new_resource.file_type)
-      set_fcontext(nil, delete_recursive_only_if, '-d', '', '(/.*)?')
+      delete_recursive_only_if = fcontext_defined("#{new_resource.file_spec}(/.*)?",
+                                                  new_resource.file_type)
+      set_fcontext(nil,
+                   delete_recursive_only_if,
+                   '-d',
+                   '',
+                   '(/.*)?')
     end
 end
 
 action :delete do
-    delete_only_if = fcontext_defined(new_resource.file_spec, new_resource.file_type, new_resource.secontext)
-    set_fcontext(nil, only_if, '-d', '', '')
+    only_if = fcontext_defined(new_resource.file_spec,
+                               new_resource.file_type,
+                               new_resource.secontext)
+    set_fcontext(nil,
+                 only_if,
+                 '-d',
+                 '',
+                 '')
 
-    delete_recursive_only_if = fcontext_defined("#{new_resource.file_spec}(/.*)?", new_resource.file_type, new_resource.secontext)
-    set_fcontext(nil, delete_recursive_only_if, '-d', '', '(/.*)?')
+    recursive_only_if = fcontext_defined("#{new_resource.file_spec}(/.*)?",
+                                         new_resource.file_type,
+                                         new_resource.secontext)
+    set_fcontext(nil,
+                 recursive_only_if,
+                 '-d',
+                 '',
+                 '(/.*)?')
 end
 
